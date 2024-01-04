0dc1dd71b9cdd86305868a7a713407e9edcfded3
ojinikim <ojinikim@gmail.com>
2024-01-04 23:53:46 +0900

    .

--- ReactNative/weather/App.js
+++ ReactNative/weather/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Helloooooooooooo</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "gold" }}>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "",
   },
 });
